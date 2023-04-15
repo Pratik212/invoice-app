af760aea5887dcfdd8eec985083460d819b579c5
Pratik212 <pratikpalanpure212@gmail.com>
2023-04-15 20:49:40 +0530

    remove un-used fields

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,7 +2,7 @@ async function handler(req, res) {
     if (req.method === "POST") {
         const {email, name, message} = req.body;
 
-        if (!email || !email.includes('@') || !name || name.trim() === '' || !message || message.trim() === '') {
+        if (!name || name.trim() === '') {
             res.status(422).json({message: 'Invalid input.'})
             return;
         }
@@ -10,7 +10,7 @@ async function handler(req, res) {
         //Store it in a database
 
         const newMessage = {
-            email, name, message
+             name
         }
         res.status(201).json({message: 'Successfully stored message!', data: newMessage})
     }
